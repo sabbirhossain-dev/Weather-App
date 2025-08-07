faf4fdd8d8238c4d8762ba70cb36ee10cf16cb52
Md Sabbir Hossain <hossainsabbir309@gmail.com>
2025-08-07 13:32:23 +0600

    change sun icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { CiCloudSun, CiSearch } from "react-icons/ci";
 import Wind from './assets/wind.png';
 import Humidity from './assets/humidity.png';
+import sun from './assets/sun.png'
 
 
 
@@ -41,7 +42,7 @@ const App = () => {
     <>
       <div className='bg-blue-200 w-full h-screen flex justify-center'>
 
-          <div className='w-full h-screen sm:h-5/6 flex flex-col md:1/2 lg:w-1/3 bg-gradient-to-r from-blue-800 to-purple-700 p-8 md:m-10 md:rounded-lg text-white shadow-slate-400 shadow-xl '>
+          <div className='w-full h-auto sm:h-5/6 flex flex-col md:1/2 lg:w-1/3 bg-gradient-to-r from-blue-800 to-purple-700 p-8 md:m-10 md:rounded-lg text-white shadow-slate-400 shadow-xl '>
             <div className='flex w-full flex-row h-10 mt-5'>
               <input ref={inputRef} placeholder='Enter a city name' type='text' className='p-4 w-full rounded-l-xl bg-transparent border outline-none border-white placeholder:text-gray-200'></input>
               <button type='search' className='p-2 pl-4 pr-4 bg-gray-100  bg-opacity-50 flex items-center justify-center border border-white rounded-r-sm text-purple-800 hover:bg-gray-100 hover:text-blue-600 duration-300 transition-all' onClick={handleClick}>
@@ -51,7 +52,9 @@ const App = () => {
 
 
           <div className='flex flex-col justify-center items-center pt-16'>
-            <div className='text-white pb-5'><CiCloudSun size={80}/></div>  
+            <div className='text-white pb-5'>
+              <img src={sun} className='w-24 h-24'/>
+            </div>  
             <div className='flex flex-col justify-center items-center gap-1'>
               <h1 className='text-6xl font-semibold'>{weatherData.temperature}°C</h1>
               <span className='text-sm text-gray-100 pb-2'>Feels Like : {weatherData.feelsLike}°C</span>
